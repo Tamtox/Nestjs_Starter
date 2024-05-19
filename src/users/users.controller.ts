16b6113f2a89aafddccef328be3f415c97a17bc0
tamtox <39272747@protonmail.com>
2024-05-19 20:10:55 +0500

    TYpes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,11 +8,18 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserBodyDto, UpdateUserBodyDto } from './dto/user.dtos';
+import {
+  CreateUserBodyDto,
+  SignUpBodyDto,
+  UpdateUserBodyDto,
+} from './dto/user.dtos';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
+  @Post('sign-up')
+  signUp(@Body() signUpBodyDto: SignUpBodyDto) {
+    return this.usersService.create(signUpBodyDto);
+  }
   @Post()
   create(@Body() createUserDto: CreateUserBodyDto) {
     return this.usersService.create(createUserDto);
